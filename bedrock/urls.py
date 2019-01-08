3989d696da3eda3606e6cea185a9da9e5cb478f0
dhruvhacks <sridhruv@yahoo.com>
2019-01-09 04:40:48 +0530

    dashboard view created

--- bedrock/urls.py
+++ bedrock/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path, include
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+]
--- bedrock/views.py
+++ bedrock/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from . import models
+
+import datetime
 
 # Create your views here.
+def dashboard(request):
+    if request.user.is_authenticated:
+        tks = models.tasks.objects.filter(year=datetime.date.today().year)
+        context = {'tks':tks}
+        return render(request, 'bedrock/dashboard.html', context)
+    else:
+        return HttpResponseRedirect('/')
