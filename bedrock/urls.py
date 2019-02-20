b9b5b6753d0fd625405d97ee7e84c0ad44f66f9e
dhruvhacks <sridhruv@yahoo.com>
2019-02-21 02:37:36 +0530

    updated url

--- bedrock/urls.py
+++ bedrock/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard_ca'),
     path('members/', views.members, name='members'),
     path('members/delete/<int:mem_id>', views.member_delete,
          name='member_delete'),
