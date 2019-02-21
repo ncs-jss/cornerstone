155f7380b5b8fec9acae21129f395109926f0d34
dhruvhacks <sridhruv@yahoo.com>
2019-02-21 20:03:40 +0530

    Online registeration added

--- keystone/urls.py
+++ keystone/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
     path('logout', views.logout, name='logout'),
+    path('', views.online_reg, name='online_reg'),
+    # path('dashboard', views.dashboard, name='dashboard'),
+    path('register', views.temp_reg, name='temp_reg'),
+    path('temp/<int:id>', views.temp_submit, name="temp_submit"),
+    path('zeal_reg', views.register, name='register'),
 ]
